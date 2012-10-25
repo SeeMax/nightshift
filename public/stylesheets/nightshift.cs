33e48212dcba3cb463be4826605a17a644249838
Max Mearsheimer <mmearshe@gmail.com>
2012-10-25 17:04:34 -0500

    fixed css an scss file to remove font face

---
+++ public/stylesheets/nightshift.cs
@@ -0,0 +1,148 @@
+body {
+  background-color: #fefcf5;
+  text-align: center;
+  min-width: 1600px; }
+
+#top_bar {
+  position: relative;
+  margin: 0px auto;
+  width: 1600px;
+  padding: 0px 0px 60px 0px; }
+  #top_bar #clock {
+    position: absolute;
+    top: 12px;
+    left: 15px;
+    display: inline-block;
+    height: 27px;
+    width: 27px; }
+  #top_bar #moon {
+    position: absolute;
+    top: 30px;
+    left: 725px;
+    width: 120px;
+    padding: 0px 0px 0px 20px;
+    background-color: #fefcf5;
+    z-index: 100; }
+  #top_bar #icons_sm {
+    position: absolute;
+    top: 12px;
+    left: 1480px; }
+    #top_bar #icons_sm img {
+      display: inline-block; }
+
+.hairline {
+  width: 100%;
+  opacity: .75;
+  border-top: 1px solid dimgrey; }
+
+#thickline {
+  position: absolute;
+  top: 283px;
+  height: 305px;
+  width: 100%;
+  background-color: dimgrey;
+  z-index: -1; }
+
+#content {
+  margin: 0px auto;
+  color: dimgrey;
+  font-family: "landrygothicregular", sans-serif; }
+
+#header {
+  position: relative;
+  display: inline-block;
+  margin: 0px auto;
+  padding: 10px 0px; }
+  #header #name {
+    font-size: 48px;
+    margin: 0px 8px;
+    padding: 70px 0px 0px 0px; }
+  #header nav {
+    padding: 10px 0px 20px 0px;
+    display: inline-block; }
+    #header nav ul {
+      list-style-type: none;
+      text-align: left;
+      display: inline; }
+      #header nav ul li {
+        padding: 0px 30px;
+        display: inline;
+        color: dimgrey;
+        font-family: TimesNewRoman, "Times New Roman", Times, Baskerville, Georgia, serif;
+        font-size: 14px; }
+        #header nav ul li a {
+          text-decoration: none;
+          color: dimgrey; }
+  #header #image {
+    margin: 0px auto; }
+  #header p {
+    padding-top: 29px;
+    line-height: 30px;
+    font-family: "landrygothicregular", sans-serif;
+    font-size: 25px;
+    font-weight: bold;
+    line-height: 55px; }
+  #header #guy_icon {
+    padding: 70px 0px 0px 0px;
+    margin: 0px auto -20px auto;
+    width: 500px; }
+    #header #guy_icon #guy {
+      position: relative;
+      top: -40px; }
+
+.section_title {
+  line-height: 30px;
+  font-family: "landrygothicregular", sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  vertical-align: top;
+  display: inline-block;
+  margin: 25px 0px 0px 20px; }
+
+.section_image {
+  display: inline-block; }
+
+#about {
+  margin: 0px auto;
+  padding: 0px 0px 60px 0px; }
+  #about p {
+    line-height: 34px;
+    font-size: 24px;
+    font-family: TimesNewRoman, "Times New Roman", Times, Baskerville, Georgia, serif; }
+
+#schedule {
+  text-align: left;
+  height: 600px;
+  width: 1200px;
+  margin: 0px auto;
+  padding-top: 60px;
+  border: red 1px solid; }
+
+#work {
+  text-align: left;
+  height: 600px;
+  width: 1200px;
+  margin: 0px auto;
+  padding-top: 60px;
+  border: red 1px solid; }
+
+#contact {
+  text-align: left;
+  height: 600px;
+  width: 1200px;
+  margin: 0px auto;
+  padding-top: 60px;
+  border: red 1px solid; }
+  #contact #thanks {
+    float: left;
+    text-align: left; }
+    #contact #thanks p1 {
+      line-height: 30px;
+      font-family: "landrygothicregular", sans-serif;
+      font-size: 25px;
+      font-weight: bold; }
+
+#bottom_border {
+  width: 100%;
+  height: 24px;
+  background-color: dimgrey; }
